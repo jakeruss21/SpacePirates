ad25f6362ce0954e5242be0b0d6fdc9d85646576
Jacob Russell <jacob.russell82@outlook.com>
2018-08-20 13:00:24 -0400

    Create main program with base classes

---
+++ SpaceEPirate/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace SpaceEPirate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //StoryLine class
+                //Character Generator
+                //Call other classes
+                //End Credits
+            //Ship class
+                //Speed
+                //Fuel Consumption
+                //Shields/Ship degridation
+                //New ships
+            //Locations class
+                //Earth at (0,0)
+                    //Market
+                    //Ship Repair/Sale
+                //Alpha Centari (0,-5)
+                //M63 (-4.6, 5)
+            //Economy Class
+                //Credits for character
+                //Trade goods
+                //Fluxuating economy w/in each location
+            //Cargo
+                //Weight
+                //Volume
+        }
+    }
+}
---
+++ SpaceEPirate/StoryLine.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpaceEPirate
+{
+    class StoryLine
+    {
+        // Character Name Gen
+        // Credits
+        // Ship
+        // Pick starting location
+        // 
+    }
+}
