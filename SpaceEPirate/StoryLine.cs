1919dbd03847ff1636986f7e3175bebb8542b033
Brittany Duke <bhall207@icloud.com>
2018-08-21 11:30:46 -0400

    This code will insert the game storyline

--- SpaceEPirate/StoryLine.cs
+++ SpaceEPirate/StoryLine.cs
@@ -18,10 +18,17 @@ namespace SpaceEPirate
             credits = UserProfile.UserMoney(1000);
             ship = SpaceShip.BeginnerShip("SimpleSimon");
             currentPlanet = Planet.StartPlanet(0);
+            StartPoint(characterName, credits, ship, currentPlanet);
         }
 
-
-
+        static void StartPoint(string characterName, int credits, string ship, string currentPlanet)
+        {
+            Console.WriteLine($"The year was 3058, the year of the dog. {characterName} had been working with their grandfathers for some time learning the family business, wtih the hope of" +
+               $" one day taking over.Little did he know that his time was coming. {characterName}'s grandfather was mysteriously killed while on a solo mission by " +
+                $"the suspected jelous Space Bandit! It is now up to {characterName} to continue grandfather's life passion all while avenging grandfathers death");
+            Console.ReadLine();
+        }
+        
     }
 
 }
