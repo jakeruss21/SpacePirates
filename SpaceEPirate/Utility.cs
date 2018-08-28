3dbb890996465e58482ae4cf954e34ac9a1cf21e
Brittany Duke <bhall207@icloud.com>
2018-08-28 08:43:43 -0400

    declared the option int after boolean statement

--- SpaceEPirate/Utility.cs
+++ SpaceEPirate/Utility.cs
@@ -6,14 +6,15 @@ namespace SpaceEPirate
 {
     class Utility
     {
-        public static int ErrorHandler(int option = 0, int numOptions = 3)
+        public static int ErrorHandler(int numOptions = 3)
         {
             Boolean answer = false;
-
+            int option = 0;
             while (option < 1 || option > numOptions)
             {
                 do
                 {
+
                     try
                     {
                         option = int.Parse(Console.ReadLine());
